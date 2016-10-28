09d3fe1a7706c904022331f330c0d3385f2415f4
Nuno <up201405774@fe.up.pt>
2016-10-28 23:31:34 +0100

    update, classes criadas

---
+++ src/ModoAula.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+
+using namespace std;
+
+class ModoAula
+{
+private:
+
+
+
+	
+public:
+
+
+	//Constructors
+
+	ModoAula();
+
+	//Sets
+
+
+
+	//Gets
+
+
+
+};
---
+++ src/ModoLIvre.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+
+using namespace std;
+
+class ModoLivre
+{
+private:
+
+
+
+
+public:
+
+
+	//Constructors
+
+	ModoLivre();
+
+	//Sets
+
+
+
+	//Gets
+
+
+
+};
